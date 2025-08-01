2364b397c6ec821443c5df6e0512142f3017f9dc
fedexyz13 <Fedexyzbots250@gmail.com>
2025-08-01 08:26:37 -0300

    Update and rename bloqueo.js to main-menu2.js

---
+++ plugins/main-menu2.js
@@ -0,0 +1,60 @@
+import fetch from 'node-fetch';
+
+const channelRD = {
+  id: '120363402097425674@newsletter',
+  name: '🌸 SukiBot_MD Canal Oficial'
+};
+
+let handler = async (m, { conn}) => {
+  const textoDescargas = `
+*⊹˚₊‧ 🧋 𝒟𝑒𝓈𝒸𝒶𝓇𝑔𝒶𝓈 𝒫𝒶𝓈𝓉𝑒𝓁 ‧₊˚⊹*
+
+𖦹 ꒰ 📸 *Imagen & Creatividad* ꒱ 💠
+┃ ⊹.imagen <tema>
+┃ ⊹.pinterest <palabra clave>
+┃ ⊹.pindl <enlace Pinterest>
+
+𖦹 ꒰ 🎶 *Música & Audio* ꒱ 💠
+┃ ⊹.play <nombre de canción>
+┃ ⊹.ytmp3 <link YouTube>
+┃ ⊹.playaudio <título>
+┃ ⊹.spotify <link Spotify>
+
+𖦹 ꒰ 📺 *Video & Redes* ꒱ 💠
+┃ ⊹.facebook <link>
+┃ ⊹.fb <link>
+┃ ⊹.instagram <link>
+┃ ⊹.insta <link>
+┃ ⊹.igdl <link Instagram>
+┃ ⊹.tiktoks search <palabra clave>
+┃ ⊹.ytsearch <título>
+┃ ⊹.yts <palabra clave>
+
+𖦹 ꒰ 📦 *Apps & Extras* ꒱ 💠
+┃ ⊹.apkmod <nombre o enlace>
+
+╭─𓆩 *SukiBot_MD descarga con estilo pastel y glitter digital* 🌈𓆪─╯`;
+
+  const bannerURL = 'https://files.catbox.moe/cvpwkk.jpg'; // Puedes cambiar esta imagen
+  const imagenBuffer = await fetch(bannerURL).then(res => res.buffer());
+
+  await conn.sendMessage(m.chat, {
+    image: imagenBuffer,
+    caption: textoDescargas,
+    contextInfo: {
+      mentionedJid: [m.sender],
+      isForwarded: true,
+      forwardingScore: 777,
+      forwardedNewsletterMessageInfo: {
+        newsletterJid: channelRD.id,
+        serverMessageId: 100,
+        newsletterName: channelRD.name
+}
+}
+}, { quoted: m});
+};
+
+handler.command = ['menu2', 'menudl'];
+handler.tags = ['menu'];
+handler.help = ['menu2', 'menudl'];
+export default handler;
