21242572f08efed19a9755d462981436226246af
fedexyz13 <Fedexyzbots250@gmail.com>
2025-08-02 12:19:11 -0300

    Update _antiprivado.js

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -7,7 +7,7 @@ export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) 
   const bot = global.db.data.settings[this.user.jid] || {};
 if (m.chat === '120363402097425674@newsletter) return !0
   if (bot.antiPrivate && !isOwner && !isROwner) {
-    await m.reply(`🍁 holis @${m.sender.split`@`[0]}, 𝒎𝒊 𝒄𝒓𝒆𝒂𝒅𝒐𝒓 𝒂 𝒅𝒆𝒔𝒂𝒄𝒕𝒊𝒗𝒂𝒅𝒐 𝒍𝒐𝒔 𝒄𝒐𝒎𝒂𝒏𝒅𝒐𝒔 𝒆𝒏 𝒍𝒐𝒔 𝒄𝒉𝒂𝒕𝒔 𝒑𝒓𝒊𝒗𝒂𝒅𝒐𝒔 𝒆𝒍 𝒄𝒖𝒂𝒍 𝒔𝒆𝒓𝒂𝒔 𝒃𝒍𝒐𝒒𝒖𝒆𝒂𝒅𝒐, 𝒔𝒊 𝒒𝒖𝒊𝒆𝒓𝒆 𝒖𝒔𝒂𝒓 𝒍𝒐𝒔 𝒄𝒐𝒎𝒂𝒏𝒅𝒐𝒔 𝒅𝒆𝒍 𝒃𝒐𝒕 𝒕𝒆 𝒆𝒏𝒗𝒊𝒕𝒐 𝒂 𝒒𝒖𝒆 𝒕𝒆 𝒖𝒏𝒂𝒔 𝒂𝒍 𝒈𝒓𝒖𝒑𝒐 𝒑𝒓𝒊𝒏𝒄𝒊𝒑𝒂𝒍 𝒅𝒆𝒍 𝒃𝒐𝒕.\n\n${gp1}`, false, {mentions: [m.sender]});
+    await m.reply(`${emoji} Hola @${m.sender.split`@`[0]}, mi creador a desactivado los comandos en los chats privados el cual serás bloqueado, si quieres usar los comandos del bot te invito a que te unas al grupo principal del bot.\n\n${gp1}`, false, {mentions: [m.sender]});
     await this.updateBlockStatus(m.chat, 'block');
   }
   return !1;
