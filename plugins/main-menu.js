04e148258771bc670fe468b510aa0c05734e9d19
fedexyz13 <Fedexyzbots250@gmail.com>
2025-08-02 10:39:42 -0300

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -38,8 +38,7 @@ const defaultMenu = {
 
 🌸 Perfil de usuario 🌸
 👤 Nombre: *%name*
-🍁 Edad: *${user.age}* años kawaii
-🌐 Pais: *${user.country}*
+🎀 Nivel: *%level* | ✨ Exp: *%exp/%maxexp*
 🔓 Modo: *%mode*
 📈 Registro global: *%totalreg*
 🕐 Tiempo activo: *%muptime*
@@ -155,4 +154,4 @@ function clockString(ms) {
   let m = isNaN(ms)? '--': Math.floor(ms / 60000) % 60;
   let s = isNaN(ms)? '--': Math.floor(ms / 1000) % 60;
   return [h, m, s].map(v => v.toString().padStart(2, '0')).join(':');
-                         }
+  }
