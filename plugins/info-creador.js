dadd44fbe43abf411aea62bb9595ca25645d7d94
fedexyz13 <Fedexyzbots250@gmail.com>
2025-08-06 23:31:45 -0300

    Update info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,91 +1,62 @@
-import PhoneNumber from 'awesome-phonenumber';
+import PhoneNumber from 'awesome-phonenumber'
 
-let handler = async (m, { conn}) => {
-  const nombreBot = 'ꜱᴜᴋɪ_ʙᴏᴛ_ᴍᴅ';
-  const creador = 'ꜰᴇᴅᴇxʏᴢ';
-  const nomorown = '5491156178758'; // número del creador
-  const dev = 'ꜰᴇᴅᴇxʏᴢ'';
-  const packname = '🌸 SukiBot_MD';
-  const yt = 'https://youtube.com/@fedexyz';
-  const github = 'https://github.com/sukiprivado';
+let handler = async (m, { conn, usedPrefix, text, args, command }) => {
+//m.react('⚙️')
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let pp = await conn.profilePictureUrl(who).catch(_ => 'https://files.catbox.moe/rkvuzb.jpg')
+let biografia = await conn.fetchStatus('584120346669' +'@s.whatsapp.net').catch(_ => 'Sin Biografía')
+let biografiaBot = await conn.fetchStatus(`${conn.user.jid.split('@')[0]}` +'@s.whatsapp.net').catch(_ => 'Sin Biografía')
+let bio = biografia.status?.toString() || 'Sin Biografía'
+let biobot = biografiaBot.status?.toString() || 'Sin Biografía'
+let name = await conn.getName(who)
 
-  const who = m.mentionedJid?.[0] || m.fromMe? conn.user.jid: m.sender;
-  const pp = await conn.profilePictureUrl(who).catch(_ => 'https://files.catbox.moe/rkvuzb.jpg');
-  const bioCreador = await conn.fetchStatus(nomorown + '@s.whatsapp.net').catch(_ => 'Sin Biografía');
-  const bioBot = await conn.fetchStatus(conn.user.jid).catch(_ => 'Sin Biografía');
+  await sendContactArray(conn, m.chat, [
+     [`${nomorown}`, `👑 Propietario`, `✨ ꜰᴇᴅᴇxʏᴢ`, dev, 'fedelanyt20@gmail.com', `🇦🇷 Argentina`, `${global.yt}`, bio],
+[`${conn.user.jid.split('@')[0]}`, `Es Un Bot 🌟`, `${packname}`, `📵 No Hacer Spam`, 'fedelanyt20@gmail.com', `🇺🇸 U.S.A`, `https://github.com/The-King-Destroy/Yuki_Suou-Bot`, biobot]
+], m)
+  //m.reply(`Hola @${m.sender.split(`@`)[0]} este es el contacto de mi creador, no hagas spam!!`)
+  } 
 
-  const bio1 = bioCreador.status?.toString() || 'Sin Biografía';
-  const bio2 = bioBot.status?.toString() || 'Sin Biografía';
-
-  const contactos = [
-    [
-      `${nomorown}`,
-      `👑 Propietario`,
-      dev,
-      '📩 fedelanyt20@gmail.com',
-      '🇻🇪 Venezuela',
-      yt,
-      bio1
-    ],
-    [
-      `${conn.user.jid.split('@')[0]}`,
-      `🤖 Bot Oficial`,
-      packname,
-      '📵 No hacer spam',
-      '📩 fedelanyt20@gmail.com',
-      '🇺🇸 U.S.A',
-      github,
-      bio2
-    ]
-  ];
-
-  await sendContactArray(conn, m.chat, contactos, m);
-  await m.react('🌸');
-};
-
-handler.help = ['creador', 'owner'];
-handler.tags = ['info'];
-handler.command = /^(owner|creador)$/i;
-export default handler;
+handler.help = ["creador","owner"]
+handler.tags = ["info"]
+handler.command = /^(owner|creador)$/i
+export default handler
 
 async function sendContactArray(conn, jid, data, quoted, options) {
-  if (!Array.isArray(data[0]) && typeof data[0] === 'string') data = [data];
-  let contacts = [];
-
-  for (let [number, name, org, email, region, link, bio] of data) {
-    number = number.replace(/[^0-9]/g, '');
-    let vcard = `
+        if (!Array.isArray(data[0]) && typeof data[0] === 'string') data = [data]
+                let contacts = []
+        for (let [number, name, isi, isi1, isi2, isi3, isi4, isi5] of data) {
+            number = number.replace(/[^0-9]/g, '')
+            let njid = number + '@s.whatsapp.net'
+            let biz = await conn.getBusinessProfile(njid).catch(_ => null) || {}
+            // N:;${name.replace(/\n/g, '\\n').split(' ').reverse().join(';')};;;
+            let vcard = `
 BEGIN:VCARD
 VERSION:3.0
 N:Sy;Bot;;;
-FN:${name}
-item.ORG:${org}
+FN:${name.replace(/\n/g, '\\n')}
+item.ORG:${isi}
 item1.TEL;waid=${number}:${PhoneNumber('+' + number).getNumber('international')}
-item1.X-ABLabel:Contacto
-item2.EMAIL;type=INTERNET:${email}
+item1.X-ABLabel:${isi1}
+item2.EMAIL;type=INTERNET:${isi2}
 item2.X-ABLabel:📧 Email
-item3.ADR:;;${region};;;;
+item3.ADR:;;${isi3};;;;
 item3.X-ABADR:ac
-item3.X-ABLabel:🌍 Región
-item4.URL:${link}
-item4.X-ABLabel:🌐 Enlace
-item5.X-ABLabel:${bio}
-END:VCARD`.trim();
-
-    contacts.push({ vcard, displayName: name});
-}
-
-  return await conn.sendMessage(
-    jid,
-    {
-      contacts: {
-        displayName: contacts.length> 1? '🌸 Contactos mágicos': contacts[0].displayName,
-        contacts
-}
-},
-    {
-      quoted,
-...options
-}
-);
-}
+item3.X-ABLabel:🏷 Region
+item4.URL:${isi4}
+item4.X-ABLabel:Website
+item5.X-ABLabel:${isi5}
+END:VCARD`.trim()
+            contacts.push({ vcard, displayName: name })
+        }
+        return await conn.sendMessage(jid, {
+            contacts: {
+                displayName: (contacts.length > 1 ? `2013 kontak` : contacts[0].displayName) || null,
+                contacts,
+            }
+        },
+        {
+            quoted,
+            ...options
+        })
+  }
