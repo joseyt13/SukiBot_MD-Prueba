c3d9f5275db8720f6245b5b848b540a0b9ab61c2
fedexyz13 <Fedexyzbots250@gmail.com>
2025-08-09 11:17:36 -0300

    Update main-allfake.js

--- plugins/main-allfake.js
+++ plugins/main-allfake.js
@@ -1,145 +1,179 @@
-import pkg from '@whiskeysockets/baileys'
-import fs from 'fs'
-import fetch from 'node-fetch'
-import axios from 'axios'
-import moment from 'moment-timezone'
+import pkg from '@whiskeysockets/baileys';
+import fs from 'fs';
+import fetch from 'node-fetch';
+import axios from 'axios';
+import moment from 'moment-timezone';
 import getMensajeSistema from '../lib/msmwarning.js';
-const { generateWAMessageFromContent, prepareWAMessageMedia, proto } = pkg
 
-var handler = m => m
-handler.all = async function (m) {
+const { generateWAMessageFromContent, prepareWAMessageMedia, proto} = pkg;
 
-global.getBuffer = async function getBuffer(url, options) {
-try {
-options ? options : {}
-var res = await axios({
-method: "get",
-url,
-headers: {
-'DNT': 1,
-'User-Agent': 'GoogleBot',
-'Upgrade-Insecure-Request': 1
+let handler = m => m;
+
+handler.all = async function (m, { conn}) {
+  if (!conn) return;
+
+  // Función para obtener buffer desde URL
+  global.getBuffer = async function getBuffer(url, options = {}) {
+    try {
+      const res = await axios({
+        method: 'get',
+        url,
+        headers: {
+          'DNT': 1,
+          'User-Agent': 'GoogleBot',
+          'Upgrade-Insecure-Request': 1
 },
 ...options,
-responseType: 'arraybuffer'
-})
-return res.data
+        responseType: 'arraybuffer'
+});
+      return res.data;
 } catch (e) {
-console.log(`Error : ${e}`)
-}}
+      console.log(`Error en getBuffer: ${e}`);
+      return null;
+}
+};
 
-// IᖴᑎᖴOᖇᗰᗩᑕIOᑎ ᗪᗴ ᒪᗩ ᗷOT
-global.creador = 'Wa.me/5491156178758'
-global.ofcbot = `Wa.me/${conn.user.jid.split('@')[0]}?text=✨️hl`
-global.asistencia = 'Wa.me/5491176429275'
-global.namechannel = '🍒 Sᴜᴋɪ𝐁𝐨𝐭_MD 🍁'
-global.namechannel2 = '🍒 Sᴜᴋɪ𝐁𝐨𝐭_MD 🍁'
-global.namegrupo = '🍒 Sᴜᴋɪ𝐁𝐨𝐭_MD 🍁'
-global.namecomu = '⍴᥆ᥕᥱrᥱძ ᑲᥡ 𝖿ᥱძᥱ᥊ᥡz'
-global.listo = 'listo aquí tienes tu pedido 🍁'
-global.fotoperfil = await conn.profilePictureUrl(m.sender, 'image').catch(_ => catalogo,)
+  // Datos del bot
+  global.creador = 'Wa.me/5491156178758';
+  global.ofcbot = `Wa.me/${conn.user.jid.split('@')[0]}?text=✨️hl`;
+  global.asistencia = 'Wa.me/5491176429275';
+  global.namechannel = '🍒 Sᴜᴋɪ𝐁𝐨𝐭_MD 🍁';
+  global.namegrupo = '🍒 Sᴜᴋɪ𝐁𝐨𝐭_MD 🍁';
+  global.namecomu = '⍴᥆ᥕᥱrᥱძ ᑲᥡ 𝖿ᥱძᥱ᥊ᥡz';
+  global.listo = 'listo aquí tienes tu pedido 🍁';
 
-// ᑕᗩᑎᗩᒪ ᗪᗴ ᒪᗩ ᗷOT
-global.idchannel = '120363402097425674@newsletter'
-global.canalIdM = ["120363402097425674@newsletter", "120363402097425674@newsletter"]
-global.canalNombreM = ["🍁 Sᴜᴋɪ𝐁𝐨𝐭_MD 🍒", "🍁 Sᴜᴋɪ 🍀"]
-global.channelRD = await getRandomChannel()
+  // Imagen de perfil del usuario
+  global.fotoperfil = await conn.profilePictureUrl(m.sender, 'image').catch(() => null);
 
+  // Canal
+  global.idchannel = '120363402097425674@newsletter';
+  global.canalIdM = [global.idchannel];
+  global.canalNombreM = [global.namechannel];
+  global.channelRD = await getRandomChannel();
 
-global.mensajes = getMensajeSistema();
+  // Mensajes decorativos
+  global.mensajes = getMensajeSistema();
 
-// ᖴᗴᑕᕼᗩՏ
-global.d = new Date(new Date + 3600000)
-global.locale = 'es'
-global.dia = d.toLocaleDateString(locale, {weekday: 'long'})
-global.fecha = d.toLocaleDateString('es', {day: 'numeric', month: 'numeric', year: 'numeric'})
-global.mes = d.toLocaleDateString('es', {month: 'long'})
-global.año = d.toLocaleDateString('es', {year: 'numeric'})
-global.tiempo = d.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true})
+  // Fecha y hora
+  const d = new Date(new Date + 3600000);
+  global.dia = d.toLocaleDateString('es', { weekday: 'long'});
+  global.fecha = d.toLocaleDateString('es', { day: 'numeric', month: 'numeric', year: 'numeric'});
+  global.mes = d.toLocaleDateString('es', { month: 'long'});
+  global.año = d.toLocaleDateString('es', { year: 'numeric'});
+  global.tiempo = d.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true});
 
-// ᖇᗴᗩᑕIOᑎᗴՏ ᘜᒪOᗷᗩᒪ
-global.rwait = '✨️'
-global.done = '✅'
-global.error = '✖️'
-global.msm = '⚠️'
+  // Reacciones globales
+  global.rwait = '✨️';
+  global.done = '✅';
+  global.error = '✖️';
+  global.msm = '⚠️';
 
-global.emoji0 = '૮₍ ˶•⤙•˶ ₎ა ✨️'
-global.emoji1 = '૮₍ ˶•⤙•˶ ₎ა ✨️'
-global.emoji2 = '૮₍ ˶•⤙•˶ ₎ა ✨️'
-global.emoji3 = '૮₍ ˶•⤙•˶ ₎ა ✨️'
-global.emoji4 = '૮₍ ˶•⤙•˶ ₎ა ✨️'
-global.emoji5 = '૮₍ ˶•⤙•˶ ₎ა ✨️'
-global.emojis = [emoji0, emoji2, emoji3, emoji4, emoji5].getRandom()
-global.emoji = [emoji0, emoji2, emoji3, emoji4, emoji5].getRandom()
+  // Emojis decorativos
+  const emojis = [
+    '૮₍ ˶•⤙•˶ ₎ა ✨️',
+    '૮₍ ˶•⤙•˶ ₎ა 🌸',
+    '૮₍ ˶•⤙•˶ ₎ა 🍓',
+    '૮₍ ˶•⤙•˶ ₎ა 🪷',
+    '૮₍ ˶•⤙•˶ ₎ა 💮'
+  ];
+  global.emoji = emojis[Math.floor(Math.random() * emojis.length)];
 
-global.wait = '🌸 𝗧𝗘 𝗣𝗜𝗗𝗢 𝗤𝗨𝗘 𝗠𝗘 𝗘𝗦𝗣𝗘𝗥𝗘𝗦 ʕ•ᴥ•ʔ'
-global.waitt = global.wait
-global.waittt = global.wait
-global.waitttt = global.wait
+  // Mensajes de espera
+  global.wait = '🌸 𝗧𝗘 𝗣𝗜𝗗𝗢 𝗤𝗨𝗘 𝗠𝗘 𝗘𝗦𝗣𝗘𝗥𝗘𝗦 ʕ•ᴥ•ʔ';
 
-global.edadaleatoria = ['10', '28', '20', '40', '18', '21', '15', '11', '9', '17', '25'].getRandom();
-global.user2 = m.pushName || 'Anónimo';
-global.verifyaleatorio = ['registrar', 'reg', 'verificar', 'verify', 'register'].getRandom();
+  // Datos aleatorios
+  global.edadaleatoria = ['10', '28', '20', '40', '18', '21', '15', '11', '9', '17', '25'][Math.floor(Math.random() * 11)];
+  global.user2 = m.pushName || 'Anónimo';
+  global.verifyaleatorio = ['registrar', 'reg', 'verificar', 'verify', 'register'][Math.floor(Math.random() * 5)];
 
-// ᗴᑎᒪᗩᑕᗴՏ ᘜᒪOᗷᗩᒪ
-var canal = 'https://whatsapp.com/channel/0029VajUPbECxoB0cYovo60W'  
-let canal2 = 'https://whatsapp.com/channel/0029VajUPbECxoB0cYovo60W'
-var git = 'https://github.com/El-brayan502'
-var github = 'https://github.com/El-brayan502/RoxyBot-MD' 
+  // Enlaces
+  const enlaces = [
+    'https://whatsapp.com/channel/0029VajUPbECxoB0cYovo60W',
+    'https://github.com/El-brayan502',
+    'https://github.com/El-brayan502/RoxyBot-MD'
+  ];
+  global.redes = enlaces[Math.floor(Math.random() * enlaces.length)];
 
-global.redes = [canal, canal2, git, github].getRandom()
+  // Imagen decorativa
+  const iconos = [
+    'https://files.catbox.moe/rkvuzb.jpg',
+    'https://files.catbox.moe/3ngnnn.jpg',
+    'https://files.catbox.moe/07fyj3.jpg',
+    'https://files.catbox.moe/erkz66.jpg'
+  ];
+  global.icono = iconos[Math.floor(Math.random() * iconos.length)];
 
-// Iᗰᗩᘜᗴᑎ
-let category = "imagen"
-const db = './src/database/db.json'
-const db_ = JSON.parse(fs.readFileSync(db))
-const random = Math.floor(Math.random() * db_.links[category].length)
-const randomlink = db_.links[category][random]
-const response = await fetch(randomlink)
-const rimg = await response.arrayBuffer()
-global.icons = rimg
+  // Saludo por hora
+  const hour = d.getHours();
+  const saludos = {
+    noche: 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃',
+    mañana: 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌄',
+    dia: 'Lɪɴᴅᴏ Dɪᴀ 🌤',
+    tarde: 'Lɪɴᴅᴀ Tᴀʀᴅᴇ 🌆'
+};
+  global.saludo =
+    hour < 4? saludos.noche:
+    hour < 10? saludos.mañana:
+    hour < 15? saludos.dia:
+    hour < 18? saludos.tarde:
+    saludos.noche;
 
-//• ↳ ◜𝑻𝑰𝑬𝑴𝑷𝑶 𝑹𝑷𝑮◞ • ⚔
-var ase = new Date(); var hour = ase.getHours(); switch(hour){ case 0: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 1: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 2: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 3: hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌄'; break; case 4: hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌄'; break; case 5: hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌄'; break; case 6: hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌄'; break; case 7: hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌅'; break; case 8: hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌄'; break; case 9: hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌄'; break; case 10: hour = 'Lɪɴᴅᴏ Dɪᴀ 🌤'; break; case 11: hour = 'Lɪɴᴅᴏ Dɪᴀ 🌤'; break; case 12: hour = 'Lɪɴᴅᴏ Dɪᴀ 🌤'; break; case 13: hour = 'Lɪɴᴅᴏ Dɪᴀ 🌤'; break; case 14: hour = 'Lɪɴᴅᴀ Tᴀʀᴅᴇ 🌆'; break; case 15: hour = 'Lɪɴᴅᴀ Tᴀʀᴅᴇ 🌆'; break; case 16: hour = 'Lɪɴᴅᴀ Tᴀʀᴅᴇ 🌆'; break; case 17: hour = 'Lɪɴᴅᴀ Tᴀʀᴅᴇ 🌆'; break; case 18: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 19: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 20: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 21: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 22: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 23: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break;}
-global.saludo = hour;
+  // Etiquetas
+  global.nombre = m.pushName || 'Anónimo';
+  global.taguser = '@' + m.sender.split('@')[0];
+  global.readMore = String.fromCharCode(8206).repeat(850);
 
-//tags
-global.nombre = m.pushName || 'Anónimo'
-global.taguser = '@' + m.sender.split("@s.whatsapp.net")
-var more = String.fromCharCode(8206)
-global.readMore = more.repeat(850)
+  // Contacto fake
+  let pp = null;
+  try {
+    pp = await conn.profilePictureUrl('5491156178758@s.whatsapp.net', 'image');
+} catch (e) {
+    pp = null;
+}
 
-//Fakes
-let pp = null; try { pp = await conn.profilePictureUrl('5491156178758@s.whatsapp.net', 'image') } catch (e) { pp = null } global.fkontak = { key: {participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: `120363402481697721@g.us` } : {}) }, message: { 'contactMessage': { 'displayName': `fedexyz owner 🍁`, 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;fedexyz owner🍁,;;;\nFN:DevBrayan creador\nitem1.TEL;waid=5491156178758:5491156178758\nitem1.X-ABLabel:Ponsel\nEND:VCARD`, 'jpegThumbnail': pp ? await (await fetch(pp)).buffer() : null, thumbnail: null, sendEphemeral: true }}}
+  global.fkontak = {
+    key: {
+      participant: '0@s.whatsapp.net',
+...(m.chat? { remoteJid: '120363402481697721@g.us'}: {})
+},
+    message: {
+      contactMessage: {
+        displayName: 'fedexyz owner 🍁',
+        vcard: `BEGIN:VCARD\nVERSION:3.0\nN:XL;fedexyz owner🍁,;;;\nFN:DevBrayan creador\nitem1.TEL;waid=5491156178758:5491156178758\nitem1.X-ABLabel:Ponsel\nEND:VCARD`,
+        jpegThumbnail: pp? await (await fetch(pp)).buffer(): null,
+        thumbnail: null,
+        sendEphemeral: true
+}
+}
+};
 
-global.fake = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: channelRD.id, newsletterName: channelRD.name, serverMessageId: -1 }
-}}, { quoted: m }
+  // Canal visual
+  global.rcanal = {
+    contextInfo: {
+      isForwarded: true,
+      forwardingScore: 100,
+      externalAdReply: {
+        showAdAttribution: true,
+        title: global.namechannel,
+        body: global.namecomu,
+        mediaUrl: null,
+        description: null,
+        previewType: 'PHOTO',
+        thumbnailUrl: global.icono,
+        sourceUrl: global.redes,
+        mediaType: 1,
+        renderLargerThumbnail: false
+}
+}
+};
+};
 
-
-
-
-
-global.icono = [ 
-'https://files.catbox.moe/rkvuzb.jpg',
-'https://files.catbox.moe/3ngnnn.jpg',
-'https://files.catbox.moe/rkvuzb.jpg',
-'https://files.catbox.moe/07fyj3.jpg',
-'https://files.catbox.moe/erkz66.jpg'
-].getRandom()
-
-global.rcanal = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { /*newsletterJid: channelRD.id,*/ serverMessageId: 100, /*newsletterName: channelRD.name,*/ }, externalAdReply: { showAdAttribution: true, title: textbot, body: dev, mediaUrl: null, description: null, previewType: "PHOTO", thumbnailUrl: icono, sourceUrl: redes, mediaType: 1, renderLargerThumbnail: false }, }, }}
-
-
-
-export default handler
-
-function pickRandom(list) {
-return list[Math.floor(Math.random() * list.length)]
-  }
+export default handler;
 
 async function getRandomChannel() {
-let randomIndex = Math.floor(Math.random() * canalIdM.length)
-let id = canalIdM[randomIndex]
-let name = canalNombreM[randomIndex]
-return { id, name }
+  const index = Math.floor(Math.random() * global.canalIdM.length);
+  return {
+    id: global.canalIdM[index],
+    name: global.canalNombreM[index]
+};
 }
