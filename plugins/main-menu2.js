69936b513c4c57a89304e05b2fab6bfba07f0140
fedexyz13 <Fedexyzbots250@gmail.com>
2025-08-11 12:11:47 -0300

    Update main-menu2.js

--- plugins/main-menu2.js
+++ plugins/main-menu2.js
@@ -4,34 +4,33 @@ let handler = async (m, { conn}) => {
   const textoDescargas = `
 *⊹˚₊‧ 🧋 𝒟𝑒𝓈𝒸𝒶𝓇𝑔𝒶𝓈 𝒫𝒶𝓈𝓉𝑒𝓁 ‧₊˚⊹*
 
-𖦹 ꒰ 📸 *Imagen & Creatividad* ꒱ 💠
-┃ ⊹.imagen <tema>
-┃ ⊹.pinterest <palabra clave>
-┃ ⊹.pindl <enlace Pinterest>
+𖦹 ꒰ 🎨 *Imagen & Creatividad* ꒱ 💠
+┃ ⊹ *.imagen* <tema>
+┃ ⊹ *.pinterest* <palabra clave>
+┃ ⊹ *.pindl* <enlace Pinterest>
 
 𖦹 ꒰ 🎶 *Música & Audio* ꒱ 💠
-┃ ⊹.play <nombre de canción>
-┃ ⊹.ytmp3 <link YouTube>
-┃ ⊹.playaudio <título>
-┃ ⊹.spotify <link Spotify>
+┃ ⊹ *.play* <nombre de canción>
+┃ ⊹ *.ytmp3* <link YouTube>
+┃ ⊹ *.playaudio* <título>
+┃ ⊹ *.spotify* <link Spotify>
 
 𖦹 ꒰ 📺 *Video & Redes* ꒱ 💠
-┃ ⊹.facebook <link>
-┃ ⊹.fb <link>
-┃ ⊹.instagram <link>
-┃ ⊹.insta <link>
-┃ ⊹.igdl <link Instagram>
-┃ ⊹.tiktoks search <palabra clave>
-┃ ⊹.ytsearch <título>
-┃ ⊹.yts <palabra clave>
+┃ ⊹ *.facebook* <link>
+┃ ⊹ *.instagram* <link>
+┃ ⊹ *.insta* <link>
+┃ ⊹ *.igdl* <link Instagram>
+┃ ⊹ *.tiktoks search* <palabra clave>
+┃ ⊹ *.ytsearch* <título>
+┃ ⊹ *.yts* <palabra clave>
 
 𖦹 ꒰ 📦 *Apps & Extras* ꒱ 💠
-┃ ⊹.apkmod <nombre o enlace>
-┃ ⊹.apk <nombre>
+┃ ⊹ *.apkmod* <nombre o enlace>
+┃ ⊹ *.apk* <nombre>
 
 ╰─𓆩 *SukiBot_MD descarga con estilo pastel y glitter digital* 🌈𓆪─╯`.trim();
 
-  const bannerURL = 'https://files.catbox.moe/cvpwkk.jpg';
+  const bannerURL = 'https://files.catbox.moe/rkvuzb.jpg';';
   const imagenBuffer = await fetch(bannerURL).then(res => res.buffer());
 
   const buttons = [
