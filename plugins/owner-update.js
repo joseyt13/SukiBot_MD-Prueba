6e9d4241f36f0f255deb8eae6f8633155a12d691
fedexyz13 <Fedexyzbots250@gmail.com>
2025-08-14 15:00:28 -0300

    Update owner-update.js

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -3,8 +3,12 @@ import { execSync} from 'child_process';
 let handler = async (m, { conn, args}) => {
   try {
     const output = execSync('git pull' + (args.length? ' ' + args.join(' '): '')).toString();
+    const now = new Date();
+    const fecha = now.toLocaleDateString('es-ES', { day: '2-digit', month: '2-digit', year: 'numeric'});
+    const hora = now.toLocaleTimeString('es-ES', { hour: '2-digit', minute: '2-digit'});
+
     let response = output.includes('Already up to date')
-? '🔧 _𝖤𝗅 𝖻𝗈𝗍 𝗒𝖺 𝖾𝗌𝗍𝖺́ 𝖺𝖼𝗍𝗎𝖺𝗅𝗂𝗓𝖺𝖽𝗈 𝖼𝗈𝗇 𝗅𝖺 𝗎́𝗅𝗍𝗂𝗆𝖺 𝗍𝖾𝗋𝗇𝗎𝗋𝖺 𝗉𝖺𝗌𝗍𝖾𝗅𝖼𝗈𝗋𝖾_'
+? '🔧 _𝖤𝗅 𝖻𝗈𝗍 𝗒𝖺 𝖾𝗌𝗍𝖺́ 𝖺𝖼𝗍𝗎𝖺𝗅𝗂𝗓𝖺𝖽𝖮 𝖼𝗈𝗇 𝗅𝖺 𝗎́𝗅𝗍𝗂𝗆𝖺 𝗍𝖾𝗋𝗇𝗎𝗋𝖺 𝗉𝖺𝗌𝗍𝖾𝗅𝖼𝗈𝗋𝖾_'
 : `*⚙ 𝗖𝗢𝗠𝗣𝗟𝗘𝗧𝗔𝗡𝗗𝗢 𝗔𝗖𝗧𝗨𝗔𝗟𝗜𝗭𝗔𝗖𝗜𝗢́𝗡...*\n\n\`\`\`\n${output}\n\`\`\``;
 
     await conn.sendMessage(m.chat, {
@@ -12,7 +16,7 @@ let handler = async (m, { conn, args}) => {
       contextInfo: {
         externalAdReply: {
           title: '𝖲𝗎𝗄𝗂Bot_MD',
-          body: '🔧 𝖲𝗎𝗄𝗂𝖻𝗈𝗍_𝗆𝖽 𝖺𝖼𝗍𝗎𝖺𝗅𝗂𝗓𝖺𝖽𝖺',
+          body: `✅ Actualizado el ${fecha} a las ${hora}`,
           mediaType: 1,
           renderLargerThumbnail: true,
           thumbnailUrl: 'https://files.cloudkuimages.guru/images/rgelVn5i.jpg',
@@ -28,7 +32,7 @@ let handler = async (m, { conn, args}) => {
       contextInfo: {
         externalAdReply: {
           title: '𝖲𝗎𝗄𝗂Bot_MD',
-          body: '⚙ 𝖲𝗎𝗄𝗂𝖻𝗈𝗍_𝗆𝖽 𝖺𝖼𝗍𝗎𝖺𝗅𝗂𝗓𝖺𝖽𝖺',
+          body: '⚠️ Error al intentar actualizar',
           mediaType: 1,
           renderLargerThumbnail: true,
           thumbnailUrl: 'https://files.cloudkuimages.guru/images/rgelVn5i.jpg',
