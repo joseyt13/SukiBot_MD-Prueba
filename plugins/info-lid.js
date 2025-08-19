3b8e2cee1f290653fd263ddfbcd27fc743d7fb5f
ꜰᴇᴅᴇxʏᴢ <Fedexyzbots250@gmail.com>
2025-08-19 20:31:12 -0300

    Update info-lid.js

--- plugins/info-lid.js
+++ plugins/info-lid.js
@@ -1,11 +1,25 @@
-let handler = async (m, { args, usedPrefix, command}) => {
-  let target = m.mentionedJid?.[0] || args[0] || m.sender
-  let id = typeof target === 'string'? target: m.sender
-
-  await m.reply(`🆔 𝗜𝗗 𝗱𝗲𝗹 𝘂𝘀𝘂𝗮𝗿𝗶𝗼:\n\n\`${id}\``)
+let handler = async (m, { conn, participants, usedPrefix, command}) => {
+  if (!m.isGroup) {
+    return m.reply(`👥 *Este hechizo solo florece en los jardines grupales* 🌷`)
 }
 
-handler.help = ['lid', 'id'']
+  let lista = participants.map(p => {
+    const nombre = conn.getName(p.id)
+    return `𓆩 🆔 \`${p.id}\`\n𓆪 🧸 *Nombre:* ${nombre}`
+}).join('\n\n')
+
+  await conn.sendMessage(m.chat, {
+    text: `✨ *Lista de miembros del grupo con sus ID mágicos* ✨\n\n${lista}`,
+    footer: 'SᴜᴋɪBot_MD 🍓',
+    buttons: [
+      { buttonId: '.menu', buttonText: { displayText: '🍁 Ver Menú'}, type: 1}
+    ],
+    headerType: 1
+}, { quoted: m})
+}
+
+handler.help = ['lid']
 handler.tags = ['info']
-handler.command = ['lid', 'id'] // puedes usar cualquiera de estos
+handler.command = ['lid']
+
 export default handler
