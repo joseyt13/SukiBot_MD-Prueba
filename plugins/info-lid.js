af3a71aa21a6f0c558c8379a109b156a232dcfc2
ꜰᴇᴅᴇxʏᴢ <Fedexyzbots250@gmail.com>
2025-08-19 20:37:41 -0300

    Update info-lid.js

--- plugins/info-lid.js
+++ plugins/info-lid.js
@@ -1,20 +1,26 @@
 let handler = async (m, { conn, participants, usedPrefix, command}) => {
   if (!m.isGroup) {
-    return m.reply(`👥 *Este hechizo solo florece en los jardines grupales* 🌷`)
+    return conn.sendMessage(m.chat, {
+      image: { url: 'https://files.catbox.moe/rkvuzb.jpg'},
+      caption: `👥 *Este hechizo solo florece en los jardines grupales de Sᴜᴋɪ* 🌷`,
+      footer: 'ꜰᴇᴅᴇxʏᴢ 🍁',
+      buttons: [{ buttonId: '.menu', buttonText: { displayText: '🍁 Ver Menú'}, type: 1}],
+      headerType: 4
+}, { quoted: m})
 }
 
-  let lista = participants.map(p => {
+  const total = participants.length
+  const lista = participants.map(p => {
     const nombre = conn.getName(p.id)
     return `𓆩 🆔 \`${p.id}\`\n𓆪 🧸 *Nombre:* ${nombre}`
 }).join('\n\n')
 
   await conn.sendMessage(m.chat, {
-    text: `✨ *Lista de miembros del grupo con sus ID mágicos* ✨\n\n${lista}`,
-    footer: 'SᴜᴋɪBot_MD 🍓',
-    buttons: [
-      { buttonId: '.menu', buttonText: { displayText: '🍁 Ver Menú'}, type: 1}
-    ],
-    headerType: 1
+    image: { url: 'https://files.catbox.moe/rkvuzb.jpg'},
+    caption: `✨ *Lista de ID * ✨\n\n👥 *Total de miembros:* ${total}\n\n${lista}`,
+    footer: 'ꜰᴇᴅᴇxʏᴢ 🍁',
+    buttons: [{ buttonId: '.menu', buttonText: { displayText: '🍁 Ver Menú'}, type: 1}],
+    headerType: 4
 }, { quoted: m})
 }
 
